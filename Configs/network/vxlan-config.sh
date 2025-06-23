e1a19b231451572261c10b2517806a446c7f12ae
swapno963 <swapno963@gmail.com>
2025-06-23 21:01:25 +0600

    Maintaining sepate file for vxlan setup

---
+++ Configs/network/vxlan-config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e  # stop on first error
+
+
+VXLAN_IF="vxlan0"
+VXLAN_ID='X' # Replace with your desired VXLAN ID
+REMOTE_IPS=('x.x.x.x')  # Dynamically replace with the IPs of other remote hosts
+
+# Create vxlan0 linked to the remote hosts
+sudo ip link add $VXLAN_IF type vxlan id $VXLAN_ID dev eth0 dstport 4789
+
+# Add FDB entries to know where to forward packets
+for ip in "${REMOTE_IPS[@]}"; do
+  sudo bridge fdb append to 00:00:00:00:00:00 dst $ip dev $VXLAN_IF
+done
+
+# Bring up vxlan0
+sudo ip link set $VXLAN_IF up
+
+# Attach vxlan0 to the docker bridge
+sudo brctl addif br-vxlan $VXLAN_IF
