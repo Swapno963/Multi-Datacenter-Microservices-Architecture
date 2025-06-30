0559d45578912b1f4157c3d0cde3bfecf79d53f6
Swapno Mondol <100410169+Swapno963@users.noreply.github.com>
2025-06-30 23:45:00 +0600

    Update vxlan setup code

--- Configs/network/vxlan-config.sh
+++ Configs/network/vxlan-config.sh
@@ -1,29 +1,10 @@
-#!/bin/bash
-set -e  # stop on first error
 
+sudo ip link add vxlan-demo type vxlan \
+  id 100 \
+  remote "$1" \
+  dstport 4789 \
+  dev eth0
+sudo ip link set vxlan-demo up
 
+sudo ip link set vxlan-demo master vxlan-net
 
-# Check if exactly two IPs are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <IP1> <IP2>"
-    exit 1
-fi
-
-
-VXLAN_IF="vxlan-net"
-REMOTE_IPS=("$1" "$2")  # Dynamically replace with the IPs of other remote hosts
-VXLAN_ID="$3" # Replace with your desired VXLAN ID
-
-# Create vxlan0 linked to the remote hosts
-sudo ip link add $VXLAN_IF type vxlan id $VXLAN_ID dev eth0 dstport 4789
-
-# Add FDB entries to know where to forward packets
-for ip in "${REMOTE_IPS[@]}"; do
-  sudo bridge fdb append to 00:00:00:00:00:00 dst $ip dev $VXLAN_IF
-done
-
-# Bring up vxlan0
-sudo ip link set $VXLAN_IF up
-
-# Attach vxlan0 to the docker bridge
-sudo brctl addif br-vxlan $VXLAN_IF
