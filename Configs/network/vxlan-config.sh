3addcae469079d9d2af71cb6bd62b9d9ebea8e0d
Swapno Mondol <100410169+Swapno963@users.noreply.github.com>
2025-07-01 00:00:53 +0600

    changed eth0 to enX0

--- Configs/network/vxlan-config.sh
+++ Configs/network/vxlan-config.sh
@@ -1,9 +1,9 @@
-
+#!/bin/bash
 sudo ip link add vxlan-demo type vxlan \
   id 100 \
   remote "$1" \
   dstport 4789 \
-  dev eth0
+  dev enX0
 sudo ip link set vxlan-demo up
 
 sudo ip link set vxlan-demo master vxlan-net
